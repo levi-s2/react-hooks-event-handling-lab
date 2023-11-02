5d480ff6e782958d55f176c8d3f0272fff601766
joao vieira <john.collins.vieira@gmail.com>
2023-11-02 19:14:53 -0400

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
 
+function change(e) {
+    e.preventDfault
+    console.log('Entering password...')
+    
+}
+
+
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={change} />
+        </div>
     )
 }
 
